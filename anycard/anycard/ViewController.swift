0d4ad01f74b11625dc852f9b91ea8caec1e4d8d6
charlyecastro <castroc@uw.edu>
2020-02-15 13:54:11 -0800

    Starter files

---
+++ anycard/anycard/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  anycard
+//
+//  Created by Charlye Castro on 2/15/20.
+//  Copyright © 2020 Charlye Castro. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+
+}
