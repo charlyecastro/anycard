dc5b4cc26005698383109c183489896428be2f03
charlyecastro <castroc@uw.edu>
2020-02-16 12:14:28 -0800

    added starter UI to settings

--- anycard/anycard/SettingsViewController.swift
+++ anycard/anycard/SettingsViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var rankPicker: UIPickerView!
+    var rankList = [""]
+        
+    @IBOutlet weak var suitLabel: UILabel!
+    @IBOutlet weak var suitPicker: UIPickerView!
+    var suitList = [""]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        if pickerView == rankPicker {
+//            return rankList.count
+//        } else {
+            return suitList.count
+       // }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        if pickerView == rankPicker {
+//            return rankList[row]
+//        } else {
+            return suitList[row]
+     //   }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        if pickerView == rankPicker {
+//            rankLabel.text = rankList[row]
+//        } else {
+            suitLabel.text = suitList[row]
+        //}
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        rankList = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+        suitList = ["Heart", "Spade", "Diamond", "Club"]
     }
     
     @IBAction func closeSettings(_ sender: Any) {
