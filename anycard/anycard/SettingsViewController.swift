06f5e1a72d43b1242e33596f640f0f9e808c9fd4
charlyecastro <castroc@uw.edu>
2020-02-16 15:06:51 -0800

    finish setting up pickerview and added image change functionality

--- anycard/anycard/SettingsViewController.swift
+++ anycard/anycard/SettingsViewController.swift
@@ -8,53 +8,69 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var cardImage: UIImageView!
     @IBOutlet weak var rankLabel: UILabel!
     @IBOutlet weak var rankPicker: UIPickerView!
-    var rankList = [""]
+    private let rankList = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
+
         
     @IBOutlet weak var suitLabel: UILabel!
     @IBOutlet weak var suitPicker: UIPickerView!
-    var suitList = [""]
+    private let suitList = ["Heart", "Spade", "Diamond", "Club"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.rankPicker.delegate = self as UIPickerViewDelegate
+        self.rankPicker.dataSource = self as UIPickerViewDataSource
+        
+        self.suitPicker.delegate = self as UIPickerViewDelegate
+        self.suitPicker.dataSource = self as UIPickerViewDataSource
+        
+       
+    }
+    
+    @IBAction func handleSave(_ sender: UIButton) {
+        let imageName = rankLabel.text! + suitLabel.text!.prefix(1)
+        print(imageName)
+        cardImage.image = UIImage(named: imageName)
+        
+    }
+    @IBAction func closeSettings(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        if pickerView == rankPicker {
-//            return rankList.count
-//        } else {
+        if pickerView.tag == 1 {
+            return rankList.count
+        } else {
             return suitList.count
-       // }
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        if pickerView == rankPicker {
-//            return rankList[row]
-//        } else {
+        if pickerView.tag == 1 {
+            return rankList[row]
+        } else {
             return suitList[row]
-     //   }
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        if pickerView == rankPicker {
-//            rankLabel.text = rankList[row]
-//        } else {
+        if pickerView.tag == 1 {
+            rankLabel.text = rankList[row]
+        } else {
             suitLabel.text = suitList[row]
-        //}
+        }
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        rankList = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
-        suitList = ["Heart", "Spade", "Diamond", "Club"]
-    }
-    
-    @IBAction func closeSettings(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
 }
+
+
+    
+    
