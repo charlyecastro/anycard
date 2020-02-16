10b7c08fd934e5d519fc06e58c1491cafb8c67ec
charlyecastro <castroc@uw.edu>
2020-02-15 23:17:35 -0800

    added settings view controller and navigation

---
+++ anycard/anycard/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  anycard
+//
+//  Created by Charlye Castro on 2/15/20.
+//  Copyright © 2020 Charlye Castro. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeSettings(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
